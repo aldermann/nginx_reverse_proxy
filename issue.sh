9fe9354c70eda4baeff065c9010d885da1e9ae76
aldermann <vietanisme@gmail.com>
2020-02-09 23:34:23 +0700

    Remove keystone.js from here
    From now on this will only contain nginx and let's encrypt
    configuration.

---
+++ issue.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo"
+  exit
+fi
+echo "After issuing finish please ctrl-C"
+sleep 1
+docker-compose -f issue-compose.yml up certbot_issue nginx_issue
+docker-compose down
+openssl dhparam -out config/certbot/conf/ssl-dhparams.pem 2048
+echo "#!/bin/bash
+docker-compose -f $(pwd)/issue-compose.yml run certbot_renew" > /etc/cron.monthly/renew_cert.sh
+chmod +x /etc/cron.monthly/renew_cert.sh
--- readme.md
+++ readme.md
@@ -1,10 +1,6 @@
 # Korama Server
 ## Stuffs:
 - SSL certificate issue script
-- Keystonejs
 - All in docker
 ## Issue SSL:
 - Run [issue.sh](script/issue.sh)
-
-## Run server:
-- Run [serve.sh](script/serve.sh)
