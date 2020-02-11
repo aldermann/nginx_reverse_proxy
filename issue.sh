ab1b835125b5485e97dcb32362e84d4a77182bf1
aldermann <vietanisme@gmail.com>
2020-02-11 15:13:46 +0700

    Add more ssl config

--- issue.sh
+++ issue.sh
@@ -7,7 +7,7 @@ echo "After issuing finish please ctrl-C"
 sleep 1
 docker-compose -f issue-compose.yml up certbot_issue nginx_issue
 docker-compose down
-openssl dhparam -out config/certbot/conf/ssl-dhparams.pem 2048
+openssl dhparam -out config/certbot/conf/ssl-dhparams.pem 4096
 echo "#!/bin/bash
 docker-compose -f $(pwd)/issue-compose.yml run certbot_renew" > /etc/cron.monthly/renew_cert.sh
 chmod +x /etc/cron.monthly/renew_cert.sh
